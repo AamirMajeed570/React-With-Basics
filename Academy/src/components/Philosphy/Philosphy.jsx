e39655153ce1c6391f77e84a1e44a370c6bb2b91
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-04-07 22:30:26 +0530

    Git Configured

--- Academy/src/components/Philosphy/Philosphy.jsx
+++ Academy/src/components/Philosphy/Philosphy.jsx
@@ -100,29 +100,66 @@ const Philosphy = () => {
           </div>
         </div>
       </div> */}
-      <div className="cards">
-      <div className="card-title1">
-        <p>
-          Our <span id="para">Philosphy</span>
-        </p>
-        <p>
-          Journey to Enlightenment: Unraveling the Spiritual Tapestry of Islamic
-          Philosophy.
-        </p>
-      </div>
-        <div className="row">
-          <div className="card1">
-            <div className="image-container">
-              <img src="../../../public/Images/Quran.png" alt="" srcset="" />
+        <div className="philosphy-container">
+          <h1 className="our-philosophy">Our <span>Philosophy</span></h1>
+          <p id="phi-para">Journey to Enlightment: Unraveling the Spiritual Tapestry of Islamic Philosophy</p>
+          <div className="row">
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/Quran.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Online Learning</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
             </div>
-            <h2>Online Learning</h2>
-            <p>
-              Quran Facility refers to the memorization and recitation of the
-              Quran.Which is the holy scripture of Quran
-            </p>
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/Star.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Spirituality</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/Quran.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Service</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/prayer.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Community</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/globe.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Learn from Anywhere Anytime</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="img-icon">
+                <img src="../../../public/Images/certificate.png" alt="" srcset="" />
+              </div>
+              <div className="philosophy-title">
+                <h1>Certificate</h1>
+                <p>Quran Facility refers to the memorization and recitation of the Quran.Which is the holy Scripture of the Islam</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
     </>
   );
 };
