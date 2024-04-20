d037812489fea001011537339942bf81581a3d63
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-04-20 18:17:13 +0530

    Git Configued

--- SecondAssignment/src/components/Main.jsx
+++ SecondAssignment/src/components/Main.jsx
@@ -4,6 +4,7 @@ import Icon from "../assets/Icon.svg";
 import Vector from "../assets/Vector.svg";
 
 import "./Main.css";
+import "./Sidebar.css";
 
 const Main = () => {
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
@@ -11,7 +12,9 @@ const Main = () => {
   const toggleSidebar = () => {
     setSidebarExpanded(!sidebarExpanded);
   };
-
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
   return (
     <>
       <nav className="container">
@@ -40,15 +43,57 @@ const Main = () => {
           }`}
           onClick={toggleSidebar}
         >
-          <img src={Vector} alt="" />
+          <div className="sidebar-icon">
+            <img
+              src={Vector}
+              alt=""
+              style={{ padding: "1.1vw", background: "#27262f" }}
+            />
+          </div>
           {sidebarExpanded && (
-            <div className="card">
-              <div className="card-header">Featured</div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-              </ul>
+            <div
+              className={`navbar-questions ${
+                sidebarExpanded ? "sidebar-expanded" : ""
+              }`}
+              onClick={stopPropagation}
+            >
+              <div className="qa">
+                <h4>Q&A</h4>
+                {/* <span>An easy Way for Everyone To ask Questions</span> */}
+                <h3>X</h3>
+              </div>
+              <div className="dropdown">
+                <div className="view-dropdown">
+                  <label htmlFor="view-select">View</label>
+                  <select name="view" id="view-select">
+                    <option value="all">All</option>
+                    <option value="answered">Answered</option>
+                    <option value="my-questions">My Questions</option>
+                    <option value="unanswered">Unanswered</option>
+                  </select>
+                </div>
+                <div className="sortBy-dropdown">
+                  <label htmlFor="sortby">Sort by</label>
+                  <select name="sort" id="sortby">
+                    <option value="Upvotes">Upvotes</option>
+                    <option value="Me Too">Me Too</option>
+                    <option value="Oldest">Oldest</option>
+                    <option value="Latest">Latest</option>
+                  </select>
+                </div>
+              </div>
+              {/* Cards */}
+              <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            </div>
             </div>
           )}
         </div>
