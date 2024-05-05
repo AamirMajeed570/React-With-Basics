a30339978a24ac7ea60b69a3c8b661f5356f0e77
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-05-05 16:46:53 +0530

    React Router Dom

---
+++ 02react-router/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-violet-500 w-[100%] h-[8vh] text-center font-bold text-3xl'>
+        <h2 className='hover:translate-x-2'>This is a React Router Crash Course</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
