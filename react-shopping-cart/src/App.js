3e3862702ad085e8df3a955fce667749d1ce2f10
aamirmajeed <127582687+deepaklegitbytes@users.noreply.github.com>
2024-10-09 22:51:13 +0530

    Add Material-UI for Designing

---
+++ react-shopping-cart/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
+import { styled } from "@mui/material";
+import Badge from "@mui/material";
+import Stack from "@mui/material/Stack";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { IconButton } from "@mui/material";
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+function App() {
+  return (
+    <div className="App">
+      <div className="list">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Gallery</li>
+        </ul>
+      </div>
+      <div className="avatar">
+        <Stack>
+          <Avatar sx={{ bgcolor: deepOrange[500] }}>A</Avatar>
+        </Stack>
+        <IconButton aria-label="cart">
+      <StyledBadge badgeContent={4} color="secondary">
+        <ShoppingCartIcon />
+      </StyledBadge>
+    </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default App;
