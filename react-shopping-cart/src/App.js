1e2ee764ee6f09ab811fd18a231ff82472150c4c
aamirmajeed <127582687+deepaklegitbytes@users.noreply.github.com>
2024-10-09 23:45:34 +0530

    Add Body Images

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -1,41 +1,26 @@
-import "./App.css";
-import Avatar from "@mui/material/Avatar";
-import { deepOrange } from "@mui/material/colors";
-import { styled } from "@mui/material";
-import Badge from "@mui/material";
-import Stack from "@mui/material/Stack";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { IconButton } from "@mui/material";
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    right: -3,
-    top: 13,
-    border: `2px solid ${theme.palette.background.paper}`,
-    padding: '0 4px',
-  },
-}));
+import Navbar from "./components/Navbar";
+import { data } from "./store";
+import './App.css';
 function App() {
   return (
-    <div className="App">
-      <div className="list">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Gallery</li>
-        </ul>
-      </div>
-      <div className="avatar">
-        <Stack>
-          <Avatar sx={{ bgcolor: deepOrange[500] }}>A</Avatar>
-        </Stack>
-        <IconButton aria-label="cart">
-      <StyledBadge badgeContent={4} color="secondary">
-        <ShoppingCartIcon />
-      </StyledBadge>
-    </IconButton>
-      </div>
+        <>
+  <Navbar />
+  {data.length === 0 ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="cards-container">
+      {data.map((item, index) => (
+        <div className="cards" key={index}>
+          <div className="title">{item.name}</div>
+          <div className="image">
+            <img src={item.image} alt={item.name} />
+          </div>
+        </div>
+      ))}
     </div>
+  )}
+</>
+
   );
 }
 
